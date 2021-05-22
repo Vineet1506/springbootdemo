558ec26ffb588fbc28a57b19d6dac744d081feb2
Vineet1506 <pingvineet@gmail.com>
2021-05-22 16:25:37 +0530

    New Project

---
+++ src/test/java/com/vineet/boot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vineet.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
